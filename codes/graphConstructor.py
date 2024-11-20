e873234eee3ce1f7c625ac7fd49b9b92cb094879
Lei Zhao <dttutty@gmail.com>
2024-11-20 00:15:00 -0600

    refactor: replace experiment notebooks with reusable graph scripts

--- codes/graphConstructor.py
+++ codes/graphConstructor.py
@@ -2,13 +2,17 @@ import pandas as pd
 import networkx as nx
 import igraph
 import stellargraph as sg
+from pathlib import Path
 from scipy import io
 from stellargraph import datasets, StellarGraph
 
+DATASET_DIR = Path(__file__).resolve().parents[1] / "datasets"
+
+
 def graphloader(dataset_str):
     if dataset_str == 'Foursquare':
-        file = './dataset/dataset_WWW_friendship_new.txt'
-        node_features = pd.read_csv("./dataset/node_features_encoded.csv", index_col=0)
+        file = DATASET_DIR / "dataset_WWW_friendship_new.txt"
+        node_features = pd.read_csv(DATASET_DIR / "node_features_encoded.csv", index_col=0)
         country_degree = pd.concat([node_features['countrycode_encoded'], node_features['degree']],axis=1)
         g = nx.read_edgelist(file , nodetype = int, edgetype='Freindship')
         ig = igraph.Graph.from_networkx(g) # NetworkX to igraph 
@@ -16,7 +20,7 @@ def graphloader(dataset_str):
         node_subjects = None # Foursquare dataset has no labels
         
     elif dataset_str == 'Flickr':
-        file = io.loadmat('./dataset/Flickr.mat')
+        file = io.loadmat(DATASET_DIR / "Flickr.mat")
         G = nx.from_scipy_sparse_matrix(file['Network'])
         fea_mat = file['Attributes'].todense()
         fea_mat = pd.DataFrame(fea_mat)
@@ -44,4 +48,3 @@ def graphloader(dataset_str):
     # Error         
     except NameError:
         print("There are no dataset ' %s '" %(dataset_str))
-
